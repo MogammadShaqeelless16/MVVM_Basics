9ab7b72298491a55b68be56d9675ffc713189522
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2020-12-03 14:53:13 +0200

    update

---
+++ MVVM project/MVVM project/ViewModules/MainPageViewmodel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM_project.ViewModules
+{
+    public class MainPageViewmodule : INotifyPropertyChanged
+    {
+        private string _demoText;
+
+        public string DemoText
+        {
+            get { return _demoText; }
+            set { _demoText = value;
+                OnPropertyChanged("demo text");
+            }
+
+      
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewmodule()
+        {
+            // making an instance of a command  to call on a later time 
+            TestButtonCommand = new Command(() => testbutton());
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            var propertyHandler = PropertyChanged;
+
+           if (propertyHandler != null)
+            {
+                propertyHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        //button running in viewmodules 
+
+        public ICommand TestButtonCommand { get; set; }
+
+        private void testbutton()
+        {
+            DemoText = "bob the builder";
+        }
+
+    }
+}
---
+++ MVVM project/MVVM project/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MVVM_project.ViewModules;
+
+namespace MVVM_project
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewmodule();
+        }
+    }
+}
